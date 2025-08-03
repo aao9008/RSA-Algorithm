4bd80ab9db726193748e18b70cb947a4dbc87366
Alfredo Ormeno <ormenoandres13@gmail.com>
2025-08-03 19:58:26 +0000

    Add driver code for cpuexp

--- prototypes/Keys/main.s
+++ prototypes/Keys/main.s
@@ -24,34 +24,62 @@ main:
     SUB sp, sp, #4
     STR lr, [sp, #0]
 
-    @ Prompt: "Enter public exponent e: "
+    #--------------cprivexp function test------------------
+    # Display test message
+    LDR r0, =promptCprivTest
+    BL printf
+
+    # Prompt: "Enter public exponent e: "
     LDR r0, =promptE
     BL printf
 
-    @ Read integer into r4
+    # Read integer into r4
     LDR r0, =formatInput
     LDR r1, =e
     BL scanf
 
-    @ Prompt: "Enter totient value: "
+    # Prompt: "Enter totient value: "
     LDR r0, =promptTotient
     BL printf
 
-    @ Read integer into r5
+    # Read integer into r5
     LDR r0, =formatInput
     LDR r1, =totient
     BL scanf
 
-    @ Load arguments into r0 and r1 for cprivexp
+    # Load arguments into r0 and r1 for cprivexp
     LDR r0, =e
     LDR r0, [r0]            @ r0 = value of e
     LDR r1, =totient
     LDR r1, [r1]            @ r1 = value of totient_value
     BL cprivexp             @ r0 = private exponent (or -1)
 
-    @ Print the result
+    # Print the result
     MOV r1, r0              @ move result into r1 for printf
-    LDR r0, =formatOutput
+    LDR r0, =formatOutput1
+    BL printf
+
+    #--------------cpubexp function test------------------
+    LDR r0, =promptCpubTest
+
+    
+    # Prompt: "Enter totient value: "
+    LDR r0, =promptTotient
+    BL printf
+
+    # Read integer into r5
+    LDR r0, =formatInput
+    LDR r1, =totient
+    BL scanf
+
+    # Load arugment into r0 for cpubexp
+    LDR r0, =totient
+    LDR r0, [r0, #0]
+    BL cpubexp
+
+    # Print the result
+    MOV r1, r0 @ move result into r1 for printf
+    LDR r0, =formatOutput2
     BL printf
 
     # Pop the stack 
@@ -61,8 +89,11 @@ main:
 
 .data
     promptE:         .asciz "Enter public exponent e: "
-    promptTotient:   .asciz "Enter totient value: "
-    formatInput:     .asciz "%d"
-    formatOutput:    .asciz "Private key exponent d = %d\n"
+    promptCpubTest: .asciz "This is a test for the cpubexp funciton. Value e will be calcuated.\n"
+    promptCprivTest: .asciz "This is a test for the cprivexp function. Exponent d will be calculated.\n"
+    promptTotient: .asciz "Enter totient value: "
+    formatInput: .asciz "%d"
+    formatOutput1: .asciz "Private key exponent d = %d\n\n"
+    formatOutput2: .asciz "Public key exponent e = %d\n"
     e:               .word 0
     totient:         .word 0
