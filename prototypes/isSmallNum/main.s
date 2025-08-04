afdcdf5d0d240b107dce692f92fe8d3d1265f188
pstev9 <portiasfdc@gmail.com>
2025-08-03 20:15:24 -0500

    Changed files per feedback
    Fixed registry save to stack, added makefile, added library, and added main function per feedback.

---
+++ prototypes/isSmallNum/main.s
@@ -0,0 +1,108 @@
+#
+# Program name: isSmallNum.s
+# Author: Portia Stevenson
+# Date: 7/28/2025
+# Purpose: Given an integer, this function determines whether the 
+# the integer is as small number (less than 50), and returns a Boolean 
+# value indicating accordingly: 
+# 1 if the number is small, and 0 if the number is not small.  
+# 
+
+
+.text
+.global main
+
+main: 
+  # Program dictionary: 
+  # r4 - user-entered input number
+  # r5 - Boolean indicating whether small number or not
+  # r8 - temporary storage for calculations
+  # r0 and r1 - function arguments and results
+  # r2 and r3 - temporary storage for calculations
+
+  # push the stack
+  SUB sp, sp, #4  
+  STR lr, [sp]
+
+  # Prompt user for input 
+  LDR r0, =prompt1
+  BL printf
+
+  # Scanf/read in user input
+  LDR r0, =formatString
+  LDR r1, =num
+  BL scanf
+
+  LDR r0, =num
+  LDR r0, [r0]
+  MOV r4, r0 		// user input in r4
+
+  # Check if user number is a small number
+  MOV r0, r4
+  BL isSmallNum
+  MOV r5, r0        	// isSmallNum in r5 (1 if true, else 0)
+
+  # If user number > 50 (i.e. r5 > 50)
+  #   print output is a small number (outputSmall)
+  #   else:
+  #     print output is not a small number (outputNotSmall)
+  CMP r5, #1
+  BNE else
+    LDR r0, =outputSmall
+    MOV r1, r4
+    BL printf
+    B end_ProgIsSmallNum
+
+  else:
+    LDR r0, =outputNotSmall
+    MOV r1, r4
+    BL printf
+
+  end_ProgIsSmallNum:
+    # Pop the stack and return to the OS
+    LDR lr, [sp]
+    ADD sp, sp, #4
+    MOV pc, lr
+
+
+# isSmallNum: Function to determine whether a number is small (<50)
+# Input: int (integer, r0)
+# Output: r0 (Boolean, r0)
+
+isSmallNum:
+  
+  # Push the stack
+  SUB sp, sp, #4     
+  STR lr, [sp]    
+
+  MOV r8, r0 		// input integer in r8
+
+  # if r8<50, it is small 
+  # else: it is not small
+  CMP r8, #50
+  BGE else_notSmall
+    MOV r0, #1
+    B endIf
+    
+  else_notSmall: 
+    MOV r0, #0
+    B endIf
+
+  endIf:
+    # Pop from the stack and return to the OS
+    LDR lr, [sp]    	
+    ADD sp, sp, #4   
+    MOV pc, lr          	
+
+# End isSmallNum function
+
+
+.data
+  prompt1: .asciz "\nEnter number: "
+  formatString: .asciz "%d"
+  num: .word 0
+  outputSmall: .asciz "Your number %d is a small number.\n\n"
+  outputNotSmall:  .asciz "Your number %d is not a small number.\n\n"
+
+# End main
+
